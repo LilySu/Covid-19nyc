5c7be2d2405ffee36dab5fe7c4a3931efa12552d
LilySu <LilySu@users.noreply.github.com>
2020-03-27 15:40:32 -0400

    updated all state graphs

--- pages/index.py
+++ pages/index.py
@@ -341,16 +341,16 @@ fig1Half.update_layout(margin={"r":0,"t":40,"l":0,"b":0})
 fig1Half.update_layout(coloraxis_showscale = False, showlegend = False)
 
 #------------------------------------------------------------------------------------------COUNTY CASES
-fig_daily_county_cases = pd.read_csv("https://raw.githubusercontent.com/LilySu/Covid-19nyc/master/df_ny/county_tableMarch26.csv")
+fig_daily_county_cases = pd.read_csv("https://raw.githubusercontent.com/LilySu/Covid-19nyc/master/df_ny/county_tableMarch27.csv")
 fig_daily_county_cases = fig_daily_county_cases.head(15)
 
 
-fig_daily_county_cases = px.bar(fig_daily_county_cases, x='index', y='March 26', 
-             text='March 26', 
-             color = 'March 26',
+fig_daily_county_cases = px.bar(fig_daily_county_cases, x='index', y='March 27', 
+             text='March 27', 
+             color = 'March 27',
              height = 350,
              color_continuous_scale=px.colors.diverging.BrBG,
-             labels={'New York State Counties':'County','March 26':'March 26th Confirmed Cases'})
+             labels={'New York State Counties':'County','March 27':'March 27th Confirmed Cases'})
 fig_daily_county_cases.update_traces(texttemplate='%{text}', textposition='outside')
 fig_daily_county_cases.update_layout(
     plot_bgcolor='white',
@@ -475,7 +475,7 @@ fig_stacked_change_county_cases.update_layout(
 )
 
 annotation4 = []
-for i,j in zip(range(26), diff_from_day_before['total']):
+for i,j in zip(range(27), diff_from_day_before['total']):
   annotation4.append(
         dict(
             x=i,
@@ -543,11 +543,11 @@ fig_stacked_ny.update_layout(
 )
 
 annotation4 = []
-for i,j in zip(range(26), diff_from_day_before['total']):
+for i,j in zip(range(27), diff_from_day_before['total']):
   annotation4.append(
         dict(
             x=i,
-            y=-500,
+            y=-600,
             xref="x",
             yref="y",
             text=str(j)[:-2]+"<br> total",
@@ -1186,7 +1186,7 @@ columnTopLeft = dbc.Col(
         dcc.Graph(figure=fig_percentage_change),
         html.Center(
             children=[
-        html.H6('Data from NY State DOH on March 26, 9 am', style={'fontSize':8, 'color':'#05b9f0', 'marginTop':10, 'marginBottom':8}),
+        html.H6('Data from NY State DOH on March 27, 9 am', style={'fontSize':8, 'color':'#05b9f0', 'marginTop':10, 'marginBottom':8}),
             ]
         ),
     ],
@@ -1226,7 +1226,7 @@ columnTopRight = dbc.Col(
         ),
         html.Center(
             children=[
-            html.H6('Data from NYC DOH, last updated there on March 27, 9 pm', style={'fontSize':8, 'color':'#05b9f0', 'marginTop':30, 'marginBottom':8}),
+            html.H6('Data from NYC DOH, last updated there on March 27, 9 am', style={'fontSize':8, 'color':'#05b9f0', 'marginTop':30, 'marginBottom':8}),
             ]
         ),
     ],
