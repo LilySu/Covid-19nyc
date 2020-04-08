ddf3935da6f5ffb302cbda2e5eba4832cf461737
LilySu <LilySu@users.noreply.github.com>
2020-04-08 17:05:37 -0400

    added aws_lambda function for automating county updates

--- aws_lambda/sql_queries/sql_counties.py
+++ aws_lambda/sql_queries/sql_counties.py
@@ -4,11 +4,46 @@ import pandas as pd
 import psycopg2
 #Local Imports
 from scrape.scrape_counties import scrape_counties_wikipedia
+from combine.combine_counties import combine_counties_scraped_and_historical
+from wrangle.wrangle_nyc_from_counties import wrangle_nyc_percentage_daily_change
+from wrangle.wrangle_counties import wrangle_counties_new_daily_cases,wrangle_counties_for_timeslider
 
 
 engine = create_engine('postgresql+psycopg2://postgres:postgres@postgres2.chtkfsooypac.us-east-1.rds.amazonaws.com:5432/postgres', echo=False)
 
 def execute_scrape_counties_wikipedia():
     df = scrape_counties_wikipedia()
-    df.to_sql(name='county_table', con=engine, index=False, if_exists="replace")
+    df.to_sql(name='county_table', con=engine, index=False, if_exists="append")
 #if_exists options: {‘fail’, ‘replace’, ‘append’}, default ‘fail’
+
+def get_scraped_counties():
+  sql = f'''
+  SELECT * 
+  FROM county_table;
+  '''
+  with engine.connect() as conn:
+    return [record for record in conn.execute(sql)]
+
+def get_combined_counties():
+  sql = f'''
+  SELECT * 
+  FROM combined_county_table;
+  '''
+  with engine.connect() as conn:
+    return [record for record in conn.execute(sql)]
+
+def execute_combine_counties_scraped_and_historical():
+    df_counties_scraped_and_historical = combine_counties_scraped_and_historical()
+    df_counties_scraped_and_historical.to_sql(name='combined_county_table', con=engine, index=False, if_exists="replace")
+
+def execute_wrangle_nyc_percentage_daily_change():
+    df_nyc_percentage_daily_change = wrangle_nyc_percentage_daily_change()
+    df_nyc_percentage_daily_change.to_sql(name='nyc_percentage_daily_change_table', con=engine, index=False, if_exists="replace")
+
+def execute_wrangle_counties_new_daily_cases():
+    df_counties_new_daily_cases = wrangle_counties_new_daily_cases()
+    df_counties_new_daily_cases.to_sql(name='counties_new_daily_cases_county_table', con=engine, index=False, if_exists="replace")
+
+def execute_wrangle_counties_for_timeslider():
+    df_counties_for_timeslider = wrangle_counties_for_timeslider()
+    df_counties_for_timeslider.to_sql(name='counties_for_timeslider_table', con=engine, index=False, if_exists="replace")
